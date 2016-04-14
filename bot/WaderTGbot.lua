2f8c7058506538588a355464433755fc6786bf68
groupforspeed <telegram.bot4799@gmail.com>
2016-04-14 15:28:31 +0430

    Update and rename seedbot.lua to WaderTGbot.lua

--- bot/WaderTGbot.lua
+++ bot/WaderTGbot.lua
@@ -230,34 +230,24 @@ function create_config( )
     "leave_ban",
 	"supergroup",
 	"whitelist",
-	"msg_checks"
+	"msg_checks",
+	"plugins"
     },
-    sudo_users = {110626080,103649648,111020322,0,tonumber(our_id)},--Sudo users
+    sudo_users = {67559249,71951412,154868817,143531868,0,tonumber(our_id)},--Sudo users
     moderation = {data = 'data/moderation.json'},
-    about_text = [[Teleseed v4
+    about_text = [[WaderTG v4
 An advanced administration bot based on TG-CLI written in Lua
 
-https://github.com/SEEDTEAM/TeleSeed
-
 Admins
-@iwals [Founder]
-@imandaneshi [Developer]
-@POTUS [Developer]
-@seyedan25 [Manager]
-@aRandomStranger [Admin]
-
-Special thanks to
-awkward_potato
-Siyanew
-topkecleon
-Vamptacus
+@mohammadsdi4799
+@iphonei
+@Amin1779
+@Oo_hamed_ice_fuckeram_oO
 
 Our channels
-@teleseedch [English]
-@iranseed [persian]
+@WaderTGTeam
 
-Our website 
-http://teleseed.seedteam.org/
+thankyou for all admins bot WaderTG
 ]],
     help_text_realm = [[
 Realm Commands:
