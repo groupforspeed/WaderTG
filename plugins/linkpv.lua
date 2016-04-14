3f82ff66406ab2f7f295218afcf6084586083dce
groupforspeed <telegram.bot4799@gmail.com>
2016-04-14 16:16:18 +0430

    Create linkpv.lua

---
+++ plugins/linkpv.lua
@@ -0,0 +1,23 @@
+do
+
+function run(msg, matches)
+       if not is_momod(msg) then
+        return "only admins groups"
+       end
+	  local data = load_data(_config.moderation.data)
+      local group_link = data[tostring(msg.to.id)]['settings']['set_link']
+       if not group_link then 
+        return "create #newlink"
+       end
+         local text = "group link:\n"..group_link
+          send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
+end
+
+return {
+  patterns = {
+    "^[!/#]linkpv"
+  },
+  run = run
+}
+
+end
