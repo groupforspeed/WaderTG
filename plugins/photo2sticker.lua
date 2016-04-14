309a7affaea453cf902f821de5704e0104e58eb2
groupforspeed <telegram.bot4799@gmail.com>
2016-04-14 17:48:38 +0430

    Update photo2sticker.lua

--- plugins/photo2sticker.lua
+++ plugins/photo2sticker.lua
@@ -1,7 +1,7 @@
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = 'data/stickers/'..msg.from.id..'.webp'
+    local file = 'data/stickers/sticker.webp'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
