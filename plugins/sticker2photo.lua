c2f948faebbe66131b1fca3705f780c02ebbe511
groupforspeed <telegram.bot4799@gmail.com>
2016-04-14 18:35:46 +0430

    Update sticker2photo.lua

--- plugins/sticker2photo.lua
+++ plugins/sticker2photo.lua
@@ -1,7 +1,7 @@
 local function tosticker(msg, success, result)
   local receiver = get_receiver(msg)
   if success then
-    local file = 'data/stickers/photos.webp'
+    local file = 'data/stickers/'..msg.from.id..'.jpg'
     print('File downloaded to:', result)
     os.rename(result, file)
     print('File moved to:', file)
@@ -16,22 +16,21 @@ local function run(msg,matches)
     local receiver = get_receiver(msg)
     local group = msg.to.id
     if msg.media then
-        if msg.media.type == 'document' and is_momod(msg) and redis:get("sticker:photo") then
-          if redis:get("sticker:photo") == 'waiting' then
-            load_document(msg.id, tosticker, msg)
-          end
-        end
+      	if msg.media.type == 'document' and is_momod(msg) and redis:get("sticker:photo") then
+      		if redis:get("sticker:photo") == 'waiting' then
+        		load_document(msg.id, tosticker, msg)
+      		end
+      	end
     end
     if matches[1] == "tophoto" and is_momod(msg) then
-      redis:set("sticker:photo", "waiting")
-      return 'Please send your sticker now'
+    	redis:set("sticker:photo", "waiting")
+    	return 'Please send your sticker now'
     end
 end
 return {
   patterns = {
-  "^[!/#](tophoto)$",
-  "%[(document)%]",
+	"^[!/#](tophoto)$",
+	"%[(document)%]",
   },
   run = run,
 }
--- by channel: @WaderTGTeam
