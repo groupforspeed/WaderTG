7e6adb542680bf95e4a407c77a83bc4fc47c0a55
groupforspeed <telegram.bot4799@gmail.com>
2016-04-20 21:08:33 +0430

    Update me.lua

--- plugins/me.lua
+++ plugins/me.lua
@@ -1,13 +1,15 @@
 do
---create by RoyalTeam ID CHANNEL : @WaderTGTeam
+--create by WaderTG ID CHANNEL : @WaderTGTeam
 function run(msg, matches)
 local reply_id = msg['id']
 
 local info = '#Name : '..msg.from.first_name..'\n===================\n'
+..'#Last name : '..(msg.from.last_name or '')..'\n===================\n'
 ..'#Id : '..msg.from.id..'\n===================\n'
-..'#Username : @'..msg.from.username..'\n===================\n'
+..'#Phone : +'..(msg.from.phone or '')..'\n===================\n'
+..'#Username : telegram.me/'..(msg.from.username or '')..'\n===================\n'
 ..'#Group Id : '..msg.to.id..'\n===================\n'
-..'#Group name : '..msg.to.title..'\n===================\nchannel:@WaderTGTeam'
+..'#Group name : '..msg.to.title..'\n===================\n@WaderTGTeam'
 
 reply_msg(reply_id, info, ok_cb, false)
 end
@@ -15,7 +17,7 @@ end
 return {
 patterns = {
 "^[!/#]me"
---create by RoyalTeam ID CHANNEL : @WaderTGTeam
+--create by WaderTG ID CHANNEL : @WaderTGTeam
 },
 run = run
 }
