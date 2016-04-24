cac2e142669e74ff01a5eb22a533aa5e55ac3331
groupforspeed <telegram.bot4799@gmail.com>
2016-04-24 17:30:00 +0430

    Update linkpv.lua

--- plugins/linkpv.lua
+++ plugins/linkpv.lua
@@ -2,14 +2,14 @@ do
 
 function run(msg, matches)
        if not is_momod(msg) then
-        return "only admins groups"
+        return "Only admins Groups"
        end
 	  local data = load_data(_config.moderation.data)
       local group_link = data[tostring(msg.to.id)]['settings']['set_link']
        if not group_link then 
         return "create #newlink"
        end
-         local text = "group link:\n"..group_link
+         local text = "Group link:\n"..group_link
           send_large_msg('user#id'..msg.from.id, text.."\n", ok_cb, false)
 end
 
