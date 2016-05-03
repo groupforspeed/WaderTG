78efb2b97d362f733a086160ec753f0fa4eb0aee
groupforspeed <telegram.bot4799@gmail.com>
2016-05-03 16:05:46 +0430

    Update aparat.lua

--- plugins/aparat.lua
+++ plugins/aparat.lua
@@ -13,9 +13,9 @@ local function run(msg, matches)
   local jdat = json:decode(url)
 
   local items = jdat.videobysearch
-  text = 'ظ†طھغŒط¬ظ‡ ط¬ط³طھظˆط¬ظˆ ط¯ط± ط¢ظ¾ط§ط±ط§طھ: \n'
+  text = 'نتیجه جستوجو در آپارات: \n'
   for i = 1, #items do
-  text = text..'\n'..i..'- '..items[i].title..'  -  طھط¹ط¯ط§ط¯ ط¨ط§ط²ط¯غŒط¯: '..items[i].visit_cnt..'\n    ظ„غŒظ†ع©: aparat.com/v/'..items[i].uid
+  text = text..'\n'..i..'- '..items[i].title..'  -  تعداد بازدید: '..items[i].visit_cnt..'\n    لینک: aparat.com/v/'..items[i].uid
   end
   text = text..'\n\n@WaderTGTeam'
   return text
