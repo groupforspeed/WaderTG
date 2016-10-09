c7ff81f2b647363a76c66740d80ec109d71a64a7
groupforspeed <telegram.bot4799@gmail.com>
2016-10-09 21:25:50 +0330

    Update support.lua

--- plugins/support.lua
+++ plugins/support.lua
@@ -1,11 +1,11 @@
 do
     local function run(msg, matches)
-    local support = '1042609774' 
+    local support = '1089640234' 
     local data = load_data(_config.moderation.data)
     local name_log = user_print_name(msg.from)
         if matches[1] == 'support' then
         local group_link = data[tostring(support)]['settings']['set_link']
-    return "Support Group link:\n"..group_link
+    return "لینک گروه پشتیبانی WaderTG:\n"..group_link
     end
 end
 return {
