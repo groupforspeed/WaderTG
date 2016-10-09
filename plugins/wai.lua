e4b138aacc7103f65ce4c0e39875c9a073b57122
groupforspeed <telegram.bot4799@gmail.com>
2016-10-09 21:07:08 +0330

    Update wai.lua

--- plugins/wai.lua
+++ plugins/wai.lua
@@ -1,27 +1,26 @@
 do
 
 local function run(msg, matches)
-  if matches[1] == 'wai' then
+  if matches[1] == 'office' then
     if is_sudo(msg) then
-     --send_document(get_receiver(msg), "/home/Seed/axs/sudo.webp", ok_cb, false)
-      return "You are my father"
+    -- send_document(get_receiver(msg), "/home/Seed/axs/sudo.webp", ok_cb, false)
+      return "شما صاحب ربات WaderTG هستید"
     elseif is_owner(msg) then
-   -- send_document(get_receiver(msg), "/home/Seed/axs/owner.webp", ok_cb, false)
-      return "You are Group Owner"
+   --send_document(get_receiver(msg), "/home/Seed/axs/owner.webp", ok_cb, false)
+      return "شما مدیر اصلی گروه هستید"
     elseif is_momod(msg) then
-  --  send_document(get_receiver(msg), "/home/Seed/axs/mod.webp", ok_cb, false)
-      return "You are Group Mod"
+  --send_document(get_receiver(msg), "/home/Seed/axs/mod.webp", ok_cb, false)
+      return "شما کاربر هستید"
     else
-  --  send_document(get_receiver(msg), "/root/Tele/axs/mmbr.webp", ok_cb, false)
-      return "You are Group Member"
+ -- send_document(get_receiver(msg), "/root/Tele/axs/mmbr.webp", ok_cb, false)
+      return "شما عضو هستید"
     end
   end
 end
 
 return {
   patterns = {
-    "^[#!/]([Ww]ai)$",
-     "^([Ww]ai)$"
+    "^[#!/]([Oo]ffice)$"
     },
   run = run
 }
