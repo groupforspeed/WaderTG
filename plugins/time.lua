4c75cb0e50428e4375f00c96e1c038fe0cfb3247
groupforspeed <telegram.bot4799@gmail.com>
2016-10-09 21:14:08 +0330

    Update time.lua

--- plugins/time.lua
+++ plugins/time.lua
@@ -1,18 +1,8 @@
---[[
-#
-#    Time And Date
-#
-#    @WaderTGTeam
-# @WaderTGTeam
-#
-#
-]]
-
 function run(msg, matches)
 local url , res = http.request('http://api.gpmod.ir/time/')
 if res ~= 200 then return "No connection" end
 local jdat = json:decode(url)
-local text = '>>Time: '..jdat.FAtime..' \n>>Today: '..jdat.FAdate..'\n------------------\n>> '..jdat.ENtime..'\n>> '..jdat.ENdate.. '\n------------------\n@WaderTGTeam'
+local text = '>>ساعت: '..jdat.FAtime..' \n>>امروز '..jdat.FAdate..'\n------------------\n>>Time '..jdat.ENtime..'\n>>Today '..jdat.ENdate.. '\n------------------\n@WaderTGTeam'
 return text
 end
 return {
